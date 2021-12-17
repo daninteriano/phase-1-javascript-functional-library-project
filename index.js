0a384cb6f6be31c1ba412f0bc6cfa38a73c22394
Daniel Interiano <dannyboyint@gmail.com>
2021-12-17 13:02:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
+function standardizeInput(collection){
+    return (collection instanceof Array) ? collection.slice() : Object.values(collection)
+}
+// standardizeInput is a helper function to use with the functions that need to
+// work with either objects or arrays
+// It checks whether the input is an array and, if so, returns a copy of it;
+// otherwise, it uses JavaScript's Object.values method to return an array that
+// contains the values of the object's propertie
+
 function myEach(collection, cbFunction){
-    Object.values(collection).forEach(function(element){
-        return cbFunction(element)
-    })
-    return collection
+    let values = standardizeInput(collection)
+    for(let i = 0; i < values.length; i++){
+         cbFunction(values[i])
+    }
+    return collection;
 }
 
 function myMap(collection, cbFunction){
-    return Object.values(collection).map(function(element){
-        return cbFunction(element)})
+    let newCollection = standardizeInput(collection)
+    let newArray = [];
+    for(let i = 0; i < newCollection.length; i++){
+        newArray.push(cbFunction(newCollection[i]))
+       /* let results = cbFunction(newCollection[i])
+        newArray.push(results)*/
+    }
+    return newArray;
 }
 
-function myReduce(collection, cbFunction, accu){
-    return Object.values(collection).reduce(function(accu, value){
-        return value + cbFunction(accu)
-}, 0)
+function myReduce(collection, cbFunction, acc){
+    let newCollection = standardizeInput(collection)
+    for(let i = 0; i < newCollection.length; i++){
+        let val = 0
+        cbFunction(acc, val, newCollection[i])
+    }
 }
-
 
 function myFind(collection, predicate){
     return Object.values(collection).find(function(element){
@@ -33,11 +50,12 @@ function mySize(collection){
     return Object.values(collection).length;
 }
 
-function myFirst(array, num){
-    return array.slice(num, -1)
+function myFirst(array, stop = false){
+    //return array.slice(0, num)
+    return stop ? array.slice(0, stop): array[0]; 
 }
-function myLast(array, num){
-    return array.slice(-1, num)
+function myLast(array, start = false){
+    return start ? array.slice(array.length - start, array.length): array[array.length - 1];
 }
 
 function myKeys(object){
